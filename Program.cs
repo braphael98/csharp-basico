22cbe5e6b5403676f72a325d56a3ac9ff4d91217
Bruno Perez <braphaelperez98@gmail.com>
2025-05-29 14:55:15 -0300

    Primeiros passos, funções e brincadeiras com o Console.WriteLine

--- Program.cs
+++ Program.cs
@@ -18,14 +18,15 @@ void ShowMessage()
 
 void MenuOptions()
 {
-    Console.WriteLine("Digite 1 para registrar uma banda");
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
     Console.WriteLine("Digite 2 para mostrar todas as bandas");
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir a media de uma banda");
     Console.WriteLine("Digite -1 para sair");
-    
+
 }
 
 ShowMessage();
+MenuOptions();
 
 
